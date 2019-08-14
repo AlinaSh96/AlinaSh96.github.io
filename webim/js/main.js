8c632b1667d51b30dd18c7990e1603765b84748f
Ken Stanton <alina30081996@gmail.com>
2019-08-14 14:20:58 +0300

    second commit

--- webim/js/main.js
+++ webim/js/main.js
@@ -1,12 +1,28 @@
+
 $(function () {
 
-  $('.menu-icon').on( 'click', function () {
-    $('.menu__item').slideToggle(500, function () {
-      if ($('.menu__item').css('display') === "none") {
+
+  $(document).ready(function () {
+    var windowHeight = $(window).height();
+
+    $(document).on('scroll', function () {
+      $('.anm').each(function () {
+        var self = $(this),
+          height = self.offset().top + self.height();
+        if ($(document).scrollTop() + windowHeight >= height) {
+          self.addClass('fadeInLeftBig ')
+        }
+      });
+    });
+  });
+
+/*меню */
+  $('.menu-icon').on('click', function () {
+    $('.menu').slideToggle(300, function () {
+      if ($(this).css('display') === "none") {
         $(this).removeAttr('style');
       }
     });
-
 
     if ($('.menu-icon').html() == '<i class="fas fa-bars"></i>') {
       $(this).html('<i class="fas fa-times"></i>');
@@ -15,9 +31,19 @@ $(function () {
     }
 
   });
-
-
-
-  
-
+/* Кнопка */
+  $(window).scroll(function () {
+    if ($(this).scrollTop() != 0)
+      $('#toTop').fadeIn();
+    else
+      $('#toTop').fadeOut();
+  });
+  $('#toTop').click(function () {
+    /*$('body,html').animate({
+      scrollTop: 0
+    }, 800);*/
+  });
 });
+
+
+
